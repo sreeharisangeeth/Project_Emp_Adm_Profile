ea7292cc44a9fc1e75bf90b48185c4859ca3d972
sreeharisangeeth <sreeharisangeeth@gmail.com>
2022-10-11 15:54:54 +0530

    Added News posting feature

--- Project_Emp_Adm_Profile/Controllers/NewsFeed1Controller.cs
+++ Project_Emp_Adm_Profile/Controllers/NewsFeed1Controller.cs
@@ -40,7 +40,32 @@ namespace Project_Emp_Adm_Profile.Controllers
             return PartialView("News", listNews);
         }
 
-        public ActionResult Newsfeedlike(string nid)
+        public ActionResult CreateNews()
+        {
+            return View();
+        }
+
+        public ActionResult PostNews(string title,string description)
+        {
+            try
+            {
+                command.CommandText = "insert into NewsFeed values(@aid,@title,@desc,getdate(),0)";
+                command.Parameters.AddWithValue("@title", title);
+                command.Parameters.AddWithValue("@desc", description);
+                command.Parameters.AddWithValue("@aid", Session["AId"]);
+                connection.Open();
+                sdr = command.ExecuteReader();
+                connection.Close();
+                return RedirectToAction("Index", "Queries");
+            }
+            catch (Exception)
+            {
+                connection.Close();
+                return RedirectToAction("Index", "Queries");
+            }
+        }
+
+            public ActionResult Newsfeedlike(string nid)
         {
 
             try
@@ -68,72 +93,3 @@ namespace Project_Emp_Adm_Profile.Controllers
         }
     }
 }
-
-/*
- * static SqlConnection connection;
-        static string conStr = "server=INL408;database=ProjectDb;trusted_connection=true;";
-        static SqlCommand command;
-        static SqlDataReader sdr;
-        // GET: NewsFeed1
-        static List<NewsFeed1> listNews;
-        public ActionResult Index()
-        {
-            listNews = new List<NewsFeed1>();
-            connection = new SqlConnection(conStr);
-            try
-            {
-                command = new SqlCommand()
-                {
-                    CommandText = "select NId, AId, Topic,Descrip,NDate,countlikes from NewsFeed ",
-                    Connection = connection
-                };
-                connection.Open();
-                sdr = command.ExecuteReader();
-                if (sdr.HasRows)
-                {
-                    while (sdr.Read())
-                    {
-                        listNews.Add(new NewsFeed1 { NId = (int)sdr[0], AId = (int)sdr[1], Topic = sdr[2].ToString(), Descrip= sdr[3].ToString(), NDate = sdr.GetDateTime(4), countlikes = (int)sdr[5] });
-                    }
-                }
-                connection.Close();
-                return View(listNews);
-            }
-            catch (Exception e) 
-            {
-                return View(listNews.ToList());
-            }
-            
-        }
-        
-
-        //private ProjectDbEntities1 db = new ProjectDbEntities1();
-        public ActionResult Newsfeedlike(string nid)
-        {
-
-            try
-            {
-                connection = new SqlConnection(conStr);
-                command = new SqlCommand()
-                {
-                    CommandText = "insert into newsfeedlikes values (@nid, @eid,getdate())",
-                    Connection = connection
-                };
-                command.Parameters.AddWithValue("@nid", nid);
-                command.Parameters.AddWithValue("@eid", Session["EmpId"]);
-
-                connection.Open();
-                sdr = command.ExecuteReader();
-                connection.Close();
-                return RedirectToAction("Index", "NewsFeeds");
-            }
-
-            catch (Exception)
-            {
-                connection.Close();
-                return RedirectToAction("Index", "NewsFeeds");
-            }
-            //return nid + " " + Session["EmpId"];
-        }
-
- */
